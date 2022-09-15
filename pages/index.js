b2a9462c74704f153f0677a0f72043e309bab2e3
ichibanm <111952104+ichibanm@users.noreply.github.com>
2022-09-15 06:47:27 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,8 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
+
     <script>window.location = "https://onlyfans.com/vamore"</script>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
     </div>
   )
 }
