111b9ad14b5f08078140ebc290a4715284552d44
ichibanm <111952104+ichibanm@users.noreply.github.com>
2022-09-15 06:53:14 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-
+ <div className="container">
+      <Head>
     <script>window.location = "https://onlyfans.com/vamore"</script>
+        <title></title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <p className="description">
+          
+        </p>
+      </main>
+
+   
     </div>
   )
 }
